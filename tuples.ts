500363707cf2efdb5776bd02b73ebf38846e3a00
tamphamtest <phamtam215@gmail.com>
2023-07-02 19:32:36 +0700

    tuples

---
+++ tuples.ts
@@ -0,0 +1,6 @@
+// Tuple là kiểu dữ liệu chuỗi bao gồm nhiều các thành phần có kiểu dữ liệu khác nhau,
+// nhưng nó là không thể thay đổi được
+
+type User3 =  [string, number, boolean]
+
+let  tUser:User3 = ["abc", 56, true];
